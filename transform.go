2d8bcb51314bb3975f52bf4bb05f9b91e7147c72
artushin <artushin@gmail.com>
2016-09-03 23:23:03 -0500

    repage

--- transform.go
+++ transform.go
@@ -92,8 +92,8 @@ func (im *Image) Shave(r Rect) (*Image, error) {
 	return im.applyRectFunc("shaving", C.ImageDataFunc(C.ShaveImage), r)
 }
 
-func (im *Image) Page(r Rect) (*Image, error) {
-	return im.applyRectFunc("paging", C.ImageDataFunc(C.pageImage), r)
+func (im *Image) Repage(r Rect) (*Image, error) {
+	return im.applyFunc("paging", C.ImageDataFunc(C.repage))
 }
 
 func (im *Image) Transform(crop, image string) *Image {
